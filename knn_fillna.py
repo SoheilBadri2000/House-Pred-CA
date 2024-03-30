584bbf806eac861cf22a1ad0f3f7e5690ec00f41
SoheilBadri2000 <sb.soheilbadri@gmail.com>
2024-03-30 02:37:22 -0400

    knn_fillna

---
+++ knn_fillna.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+from sklearn.impute import KNNImputer
+
+df = pd.read_csv("data/aug/data_aug_2024-03-25.csv")
+# df = pd.read_csv("data/data_cleaned_ph2.csv")
+# df = pd.read_csv("data/data_cleaned_ph1_4.csv")
+
+df_knn = df[['bathrooms_total', 'bedrooms_extra', 'bedrooms',
+            'stories_total', 'size_interior', 'building_type',
+            'property_type', 'lng', 'lat',
+            'ownership_type_group_ids', 'parkings',
+            'province', 'price']]
+
+# df_knn = df[df["province"]=="Ontario"].drop(["agency_name", "agency_type", "property_type", "ownership_type", "land_size", "page_url", "timestamp", "postal_code"], axis=1)
+
+# set houses with size of 0 or 1 to NaN
+df_knn["size_interior"] = df_knn["size_interior"].replace([0,1], np.NaN)
+
+df_knn = pd.get_dummies(df_knn)
+columns = df_knn.columns
+
+# print(df_knn.columns)
+imputer = KNNImputer(n_neighbors=5)
+df_knn = imputer.fit_transform(df_knn)
+df_knn = pd.DataFrame(df_knn, columns=columns)
+
+print(df_knn.corr(numeric_only=True)["price"].sort_values(ascending=False))
+
+df["size_interior"] = df_knn["size_interior"]
+df.to_csv("data/knn/data_knn_2024-03-25.csv")
+
