ada08f7b55048840db0c057aace84e84224c6159
SoheilBadri2000 <sb.soheilbadri@gmail.com>
2024-04-30 00:56:14 -0400

    enc-2024-04-29

--- merge_encode_fillnna.py
+++ merge_encode_fillnna.py
@@ -2,9 +2,9 @@ import pandas as pd
 from sklearn.impute import KNNImputer
 from sklearn.neighbors import BallTree
 
-df = pd.read_csv("data/lof/data-lof-2024-04-22.csv")
-df_dem = pd.read_csv("data/LocalLogic/Demographics/demographics-2024-04-22.csv")
-df_loc = pd.read_csv("data/LocalLogic/Locations/locations-2024-04-22.csv")
+df = pd.read_csv("data/lof/data-lof-2024-04-29.csv")
+df_dem = pd.read_csv("data/LocalLogic/Demographics/demographics-2024-04-29.csv")
+df_loc = pd.read_csv("data/LocalLogic/Locations/locations-2024-04-29.csv")
 
 
 df_geo_price = df[["lng", "lat", "price"]]
@@ -81,6 +81,7 @@ if __name__ == "__main__":
 
     df = df.drop(["id", "id_mls", "agency_name", "agency_type", "property_type", "ownership_type", "land_size", "page_url", "timestamp", "postal_code"], axis=1)
 
+    df["ownership_type_group_ids"] = df["ownership_type_group_ids"].astype("int")
     df["ownership_type_group_ids"] = df["ownership_type_group_ids"].astype("object")
 
     df_enc = pd.get_dummies(df)
@@ -91,4 +92,4 @@ if __name__ == "__main__":
     df_enc = pd.DataFrame(df_enc, columns=columns)
 
 
-    df_enc.to_csv("data/enc/data-enc-2024-04-22.csv", index=False)
+    df_enc.to_csv("data/enc/data-enc-2024-04-29.csv", index=False)
